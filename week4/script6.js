f65e5474a7449cdcb25f8ed7f8065657726ab3c9
20404281 <M.FIXIMALIK411@GMAIL.COM>
2021-03-28 21:30:40 +0100

    exercises week4 javascript practise

---
+++ week4/script6.js
@@ -0,0 +1,16 @@
+function updateParagraph() {
+var element = document.getElementById('paragraph');
+element.firstChild.nodeValue = 'New Heading';
+}
+function updateH1() {
+var element = document.getElementById('pageheading');
+element.firstChild.nodeValue = 'New paragraph contents';
+}
+function myLoadFunction() {
+var element = document.getElementById('pageheading');
+element.addEventListener('click', updateH1);
+var element = document.getElementById('paragraph');
+element.addEventListener('click', updateParagraph);
+}
+
+document.addEventListener('DOMContentLoaded', myLoadFunction);
